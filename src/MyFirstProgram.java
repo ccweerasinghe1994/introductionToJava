bb622b2e8a5434cbd31c47367cfb7300079ae14e
wchamara <chamara.weerasinghe@my.sliit.lk>
2022-06-28 06:51:12 +0530

    using bytes and double and floats

--- src/MyFirstProgram.java
+++ src/MyFirstProgram.java
@@ -1,5 +1,74 @@
 public class MyFirstProgram {
-    public static void main(String[] args){
-       // System.out.println("hello world");
+    public static String job = "Dev";
+
+    public static void main(String[] args) {
+        byte varByte = 123;
+        System.out.println("the byte variable : " + varByte);
+
+        byte newVarByte = -128;
+        System.out.println("the byte newVariable : " + newVarByte);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+//        -------------------------------------     using bytes   ----------------------------------------
+        byte varByte = 123;
+        System.out.println("the byte variable : " + varByte);
+
+        byte newVarByte = -128;
+        System.out.println("the byte newVariable : " + newVarByte);
+//        -------------------------------------     using the float and double with exponents   ----------------------------------------
+//        float varFloat = 10e3f;
+//        System.out.println("number 10e3f :"+varFloat);
+//
+//        double varDouble = 10E-4d;
+//        System.out.println("number 10E -4d :"+varDouble);
+
+//        -------------------------------------     we can combine the static and local variables as well    ----------------------------------------
+      String name = "dave";
+        System.out.println(name +" is a "+job);
+
+
+//        -------------------------------------     static variables    ----------------------------------------
+        int localVar = 12;
+        System.out.println("Static variable is : "+ staticVar);
+        System.out.println("Local variable is :"+ localVar);
+
+
+//        --------  let's create multiple variables in a single line    -----------------------------------------
+
+        int x= 23,y=44,z=1;
+        System.out.println("x value is equal to: "+x);
+        System.out.println("y value is equal to: "+y);
+        System.out.println("z value is equal to: "+z);
+
+
+//        ---------     this is using format to add numbers and strings     --------------------------------------
+
+        int varInt = 100;
+        String message = "this is a message";
+        System.out.format("the integer %d is the value of the variable \n", varInt);
+        System.out.format("this is a %s", message);
+
+
+*/
     }
 }
